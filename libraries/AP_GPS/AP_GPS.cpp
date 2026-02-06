e9f7e76ac08e8a38c924d2da221168426743cbe0
zhangyihang <114992590+zhangyihangceshi1hao@users.noreply.github.com>
2026-02-06 17:04:42 +0800

    Update AP_GPS.cpp

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -1039,7 +1039,7 @@ void AP_GPS::update_instance(uint8_t instance)
 
     if (data_should_be_logged) {
         // keep count of delayed frames and average frame delay for health reporting
-        const uint16_t gps_max_delta_ms = 200; // 200 ms (5Hz) + 45 ms buffer
+        const uint16_t gps_max_delta_ms = 245; // 200 ms (5Hz) + 45 ms buffer
         GPS_timing &t = timing[instance];
 
         if (t.delta_time_ms > gps_max_delta_ms) {
